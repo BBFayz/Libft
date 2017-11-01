69f2e3e18cecff33eb913ad8818ff993fa2a72fa
Alex ZYBERT <azybert@e2r5p9.42.fr>
2017-11-01 08:50:12 +0100

    map & mapi

--- ft_strmap.c
+++ ft_strmap.c
@@ -6,48 +6,21 @@
 /*   By: azybert <azybert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/17 14:35:10 by azybert           #+#    #+#             */
-/*   Updated: 2017/01/27 20:36:13 by azybert          ###   ########.fr       */
+/*   Updated: 2017/11/01 08:44:37 by azybert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <string.h>
-
-char	*ft_strmap2(size_t len)
-{
-	char	*mem;
-
-	mem = (char *)malloc(len + 1);
-	if (mem == NULL)
-		return (NULL);
-	return (mem);
-}
+#include "libft.h"
 
 char	*ft_strmap(char const *s, char (*f)(char))
 {
-	size_t			len;
-	char			*mem;
-	unsigned int	i;
+	int		i;
+	char	*mem;
 
-	if (s == NULL || f == NULL)
-		return (NULL);
-	mem = (char *)s;
-	len = 0;
-	while (*mem)
-	{
-		mem++;
-		len++;
-	}
-	mem = ft_strmap2(len);
-	if (mem == NULL)
+	if (f == NULL || !(mem = ft_strdup(s)))
 		return (NULL);
 	i = -1;
 	while (s[++i])
-	{
-		*mem = f(s[i]);
-		mem++;
-	}
-	*mem = '\0';
-	mem = mem - len;
+		mem[i] = f(s[i]);
 	return (mem);
 }
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,48 +6,21 @@
 /*   By: azybert <azybert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/17 14:35:10 by azybert           #+#    #+#             */
-/*   Updated: 2017/01/27 20:22:39 by azybert          ###   ########.fr       */
+/*   Updated: 2017/11/01 08:45:36 by azybert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <string.h>
+#include "libft.h"
 
-static char	*ft_strmapi2(size_t len)
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	int		i;
 	char	*mem;
 
-	mem = malloc(len + 1);
-	if (mem == NULL)
-		return (NULL);
-	return (mem);
-}
-
-char		*ft_strmapi(char const *s, char (*f)(unsigned int, char))
-{
-	size_t			len;
-	char			*mem;
-	unsigned int	i;
-
-	if (s == NULL || f == NULL)
-		return (NULL);
-	mem = (char *)s;
-	len = 0;
-	while (*mem)
-	{
-		mem++;
-		len++;
-	}
-	mem = ft_strmapi2(len);
-	if (mem == NULL)
+	if (f == NULL || !(mem = ft_strdup(s)))
 		return (NULL);
 	i = -1;
 	while (s[++i])
-	{
-		*mem = f(i, s[i]);
-		mem++;
-	}
-	*mem = '\0';
-	mem = mem - len;
+		mem[i] = f(i, s[i]);
 	return (mem);
 }
